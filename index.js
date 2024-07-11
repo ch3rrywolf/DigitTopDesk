abc86ee5c074cf35571479b902c76cd477410686
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-12 00:03:37 +0100

    authRoute

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/digit-tdsk");
 
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 app.use(express.static('public'));
 
 const port = process.env.SERVER_PORT | 3000;
