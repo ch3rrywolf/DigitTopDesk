4a79e4665efb152cf110f2485e35d51986202315
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-16 15:49:19 +0100

    commonRoutes.js delete-user

--- controllers/userController.js
+++ controllers/userController.js
@@ -168,9 +168,52 @@ const updateUser = async(req, res) => {
     }
 }
 
+const deleteUser = async(req, res) => {
+    try{
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                success: false,
+                msg: 'Errors',
+                errors: errors.array()
+            });
+        }
+
+        const { id } = req.body;
+
+        const isExists = await User.findOne({
+            _id: id
+        });
+
+        if(!isExists){
+            return res.status(400).json({
+                success: false,
+                msg: 'User not found!'
+            });
+        }
+
+        await User.findByIdAndDelete({
+            _id: id
+        });
+
+        return res.status(200).json({
+            success: true,
+            msg: 'User Deleted Successfully!'
+        });
+
+    } catch(error){
+        return res.status(400).json({
+            success: false,
+            msg: error.message
+        });
+    }
+}
 
 module.exports = {
     createUser,
     getUsers,
-    updateUser
+    updateUser,
+    deleteUser
 }
