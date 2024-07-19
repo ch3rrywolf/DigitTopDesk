1e93100a5f738fea83354173f7e91305586cdcec
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-19 16:12:07 +0100

    checkPermission.js

--- middlewares/checkPermission.js
+++ middlewares/checkPermission.js
@@ -2,7 +2,15 @@ const checkPermission = async(req, res, next) => {
 
     try{
 
-        console.log(req.user);
+        if(req.user.role != 1){ // if user not admin
+            return res.status(400).json({
+                success: false,
+                msg: 'We will work on next lecture!'
+            });
+        }
+
+        return next();
+        
     } catch(error){
         return res.status(400).json({
             success: false,
