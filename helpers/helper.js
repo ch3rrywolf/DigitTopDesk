7624703b53ea518af422c060b03bab7e3ad37640
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-19 15:19:19 +0100

    init helper.js getUserPermissions

---
+++ helpers/helper.js
@@ -0,0 +1,13 @@
+const User = require('../models/userModel');
+
+const getUserPermissions = async(user_id) => {
+    try{
+
+    } catch(error){
+        console.log(error.message);
+    }
+}
+
+module.exports = {
+    getUserPermissions
+}
