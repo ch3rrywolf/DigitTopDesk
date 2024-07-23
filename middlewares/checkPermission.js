f502d3d37e9240637e10b233815d8dba91e8f8f7
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-23 11:17:11 +0100

    check user permission with router permission & middleware testing

--- middlewares/checkPermission.js
+++ middlewares/checkPermission.js
@@ -7,8 +7,24 @@ const checkPermission = async(req, res, next) => {
         if(req.user.role != 1){ // if user not admin
             
             const routerPermission = await helper.getRouterPermission(req.path, req.user.role);
+            const userPermissions = await helper.getUserPermissions(req.user._id);
 
-            if(!routerPermission){
+            if(userPermissions.permissions.permissions == undefined || !routerPermission){
+                return res.status(400).json({
+                    success: false,
+                    msg: "You have n't permission to access this route!"
+                });
+            }
+
+            const permission_name = routerPermission.permission_id.permission_name;
+            const permission_values = routerPermission.permission;
+
+            const hasPermission = userPermissions.permissions.permissions.some(permission =>
+                permission.permission_name == permission_name &&
+                permission.permission_value.some(value => permission_values.includes(value))
+            );
+
+            if(!hasPermission){
                 return res.status(400).json({
                     success: false,
                     msg: "You have n't permission to access this route!"
